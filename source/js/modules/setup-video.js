5d7e74bc16f93880ba6989e3b613b64d4cdd48fd
IrisDev11 <iris@dostavista.ru>
2023-05-12 21:34:38 +0300

    Завершает главный блок и блок тренажерный зал

---
+++ source/js/modules/setup-video.js
@@ -0,0 +1,29 @@
+const video = document.querySelector('.about-company__video');
+
+const setupVideo = () => {
+  let link = video.querySelector('.about-company__video-link');
+  let button = video.querySelector('.about-company__video-button');
+
+  const createIframe = () => {
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw');
+
+    iframe.classList.add('about-company__iframe');
+
+    return iframe;
+  };
+
+  video.addEventListener('click', () => {
+    let iframe = createIframe();
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+};
+
+export {setupVideo};
